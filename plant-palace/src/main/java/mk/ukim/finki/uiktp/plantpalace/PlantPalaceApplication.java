6aab532bdc1890609fa66ba5d3a6264161af34f7
Boris Djotov <borisdzotov@gmail.com>
2024-03-29 14:49:09 +0100

    Initial Spring project created

---
+++ plant-palace/src/main/java/mk/ukim/finki/uiktp/plantpalace/PlantPalaceApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.uiktp.plantpalace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantPalaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlantPalaceApplication.class, args);
+    }
+
+}
