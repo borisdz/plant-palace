71c163497127b7ab6ad6671fd032916e37412f7e
Boris Djotov <borisdzotov@gmail.com>
2024-05-17 20:57:55 +0200

    Added @Data and removed getters and setters in Category.java

--- plant-palace/src/main/java/mk/ukim/finki/uiktp/plantpalace/model/Category.java
+++ plant-palace/src/main/java/mk/ukim/finki/uiktp/plantpalace/model/Category.java
@@ -1,8 +1,10 @@
 package mk.ukim.finki.uiktp.plantpalace.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Category {
 
     @Id
@@ -23,30 +25,6 @@ public class Category {
 
     public Category(String name, Category parent) {
         this.name = name;
-        this.parent = parent;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Category getParent() {
-        return parent;
-    }
-
-    public void setParent(Category parent) {
         this.parent = parent;
     }
 }
